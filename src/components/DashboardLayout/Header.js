6daf75df9abb33aef84972df6dae1da11ce46506
sushilAdwe <ssubhash@iitg.ac.in>
2022-05-22 15:38:14 +0530

    header

--- src/components/DashboardLayout/Header.js
+++ src/components/DashboardLayout/Header.js
@@ -421,10 +421,7 @@ function Header({
               </div>
             </div>
           </Drawer>
-          <Link to="/sign-in" className="btn-sign-in">
-            {profile}
-            <span>Sign in</span>
-          </Link>
+
           <Input
             className="header-search"
             placeholder="Type here..."
